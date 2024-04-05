683a8509f177f616d1e32b4e1988ff11a9038467
MineGame159 <petulko08@gmail.com>
2024-04-05 17:00:31 +0200

    Move to a 'fabric' subproject. Use the Kotlin Gradle DSL.

---
+++ build.gradle.kts
@@ -0,0 +1,4 @@
+subprojects {
+    group = project.property("maven_group").toString()
+    version = project.property("mod_version").toString()
+}
---
+++ fabric/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("fabric-loom") version "1.6-SNAPSHOT"
+}
+
+base {
+    archivesName = project.property("archives_base_name").toString()
+}
+
+repositories {
+    maven {
+        name = "Curse Maven"
+        url = uri("https://cursemaven.com")
+
+        content {
+            includeGroup("curse.maven")
+        }
+    }
+}
+
+dependencies {
+    // Minecraft, Fabric
+    minecraft("com.mojang:minecraft:${project.property("minecraft_version")}")
+    mappings("net.fabricmc:yarn:${project.property("yarn_mappings")}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")}")
+
+    // Fabric API
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_version")}")
+
+    // MicroHTTP
+    implementation("org.microhttp:microhttp:${project.property("microhttp_version")}")
+    include("org.microhttp:microhttp:${project.property("microhttp_version")}")
+
+    // Open Parties and Claims
+    modCompileOnly("curse.maven:open-parties-and-claims-636608:4982662")
+}
+
+tasks {
+    processResources {
+        val properties = mapOf(
+            "version" to project.version,
+            "minecraft_version" to project.property("minecraft_version"),
+            "loader_version" to project.property("loader_version")
+        )
+
+        inputs.properties(properties)
+
+        filesMatching("fabric.mod.json") {
+            expand(properties)
+        }
+    }
+
+    jar {
+        from("../LICENSE") {
+            rename { "${it}_${project.base.archivesName.get()}"}
+        }
+    }
+
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.release = 17
+    }
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
